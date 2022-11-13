2368292d9a3beac168b367df15c43e2b77a7067b
yiğitcan <cakmak02273@gmail.com>
2022-11-13 17:12:01 +0300

    falan

--- hafta lab/src/App.java
+++ hafta lab/src/App.java
@@ -2,5 +2,22 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
+        System.out.println("denememeemememem");
     }
 }
